c71a39a0f2db1ea28e0c1670c349dba89072449e
Кирилл <k.otvetchikov@zebrains.team>
2026-02-12 17:15:45 +0400

    (feat)WEDDING: Прокинул CSRF_TRUSTED_ORIGINS в настройки Django

--- wedding_invitation/settings.py
+++ wedding_invitation/settings.py
@@ -18,6 +18,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
